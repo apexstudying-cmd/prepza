ca0cec1d30a45f336454effa25ae20bf82d6855e
Arnold Gichuru <299492423+apexstudying-cmd@users.noreply.github.com>
2026-09-12 21:16:18 +0300

    feat: install E2EE chat fetch bridge

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { installE2EEFetchBridge } from './crypto/e2eeFetchBridge'
 import App from './App'
 import './index.css'
+
+installE2EEFetchBridge()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
