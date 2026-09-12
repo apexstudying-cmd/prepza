9f2edc945d93d9c9408504830417fb22241d522d
Arnold Gichuru <299492423+apexstudying-cmd@users.noreply.github.com>
2026-09-12 22:52:25 +0300

    feat: mount shared study document reader

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import { installNewGroupE2EECreationGuard } from './crypto/newGroupE2EECreationG
 import { installStudyAdaFetchGuard } from './crypto/studyAdaFetchGuard'
 import { installInChatAdaObserver, default as InChatAdaEnhancer } from './crypto/inChatAdaEnhancer'
 import { installDirectInChatAdaObserver, default as DirectInChatAdaEnhancer } from './crypto/directInChatAdaEnhancer'
+import { installChatStudyDocumentObserver, default as ChatStudyDocumentReader } from './crypto/chatStudyDocumentReader'
 import App from './App'
 import './index.css'
 
@@ -15,11 +16,13 @@ installDirectChatE2EE()
 installNewGroupE2EECreationGuard()
 installInChatAdaObserver()
 installDirectInChatAdaObserver()
+installChatStudyDocumentObserver()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <InChatAdaEnhancer />
     <DirectInChatAdaEnhancer />
+    <ChatStudyDocumentReader />
   </React.StrictMode>,
 )
