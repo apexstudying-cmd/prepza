a8325417c0e81e97e6be79fddf8db824a233e067
Arnold Gichuru <299492423+apexstudying-cmd@users.noreply.github.com>
2026-09-12 22:14:30 +0300

    feat: mount in-chat Ada study experience

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client'
 import { installE2EEFetchBridge } from './crypto/e2eeFetchBridge'
 import { installNewGroupE2EECreationGuard } from './crypto/newGroupE2EECreationGuard'
 import { installStudyAdaFetchGuard } from './crypto/studyAdaFetchGuard'
+import { installInChatAdaObserver, default as InChatAdaEnhancer } from './crypto/inChatAdaEnhancer'
 import App from './App'
 import './index.css'
 
 installStudyAdaFetchGuard()
 installE2EEFetchBridge()
 installNewGroupE2EECreationGuard()
+installInChatAdaObserver()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <InChatAdaEnhancer />
   </React.StrictMode>,
 )
