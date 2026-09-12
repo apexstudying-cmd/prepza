04c197559c74698f3025fab60ad6c43be08d280f
Arnold Gichuru <299492423+apexstudying-cmd@users.noreply.github.com>
2026-09-12 22:39:21 +0300

    feat: install direct chat E2EE transport

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { installE2EEFetchBridge } from './crypto/e2eeFetchBridge'
+import { installDirectChatE2EE } from './crypto/directChatE2EE'
 import { installNewGroupE2EECreationGuard } from './crypto/newGroupE2EECreationGuard'
 import { installStudyAdaFetchGuard } from './crypto/studyAdaFetchGuard'
 import { installInChatAdaObserver, default as InChatAdaEnhancer } from './crypto/inChatAdaEnhancer'
@@ -9,6 +10,7 @@ import './index.css'
 
 installStudyAdaFetchGuard()
 installE2EEFetchBridge()
+installDirectChatE2EE()
 installNewGroupE2EECreationGuard()
 installInChatAdaObserver()
 
