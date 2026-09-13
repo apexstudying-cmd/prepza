b36c5f0b2992ef7bb706682900855707c58d71cf
Arnold Gichuru <299492423+apexstudying-cmd@users.noreply.github.com>
2026-09-13 10:31:15 +0300

    feat: connect in-chat Ada panel

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,6 +9,7 @@ import { installInChatAdaObserver, default as InChatAdaEnhancer } from './crypto
 import { installDirectInChatAdaObserver, default as DirectInChatAdaEnhancer } from './crypto/directInChatAdaEnhancer'
 import { installChatStudyDocumentObserver, default as ChatStudyDocumentReader } from './crypto/chatStudyDocumentReader'
 import WhatsAppChatExperience from './crypto/WhatsAppChatExperience'
+import WhatsAppAdaPanel from './crypto/WhatsAppAdaPanel'
 import App from './App'
 import './index.css'
 
@@ -28,6 +29,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <WhatsAppChatExperience />
+    <WhatsAppAdaPanel />
     <InChatAdaEnhancer />
     <DirectInChatAdaEnhancer />
     <ChatStudyDocumentReader />
